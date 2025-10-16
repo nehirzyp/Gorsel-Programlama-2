dd2a02c89fa688d2cdcdc10346c249801cf285fc
nehirzyp <nzyaicg@gmail.com>
2025-10-16 10:04:05 +0300

    4.Hafta Puan Oyunu

---
+++ PuanOyunu/PuanOyunu/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace PuanOyunu
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PUAN";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
---
+++ PuanOyunu/PuanOyunu/Form1.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PuanOyunu
+{
+    public partial class Form1 : Form
+    {
+        //Ortak puan
+        static int puan = 0;
+
+        bool oyunModu = false;
+
+        Button oyunButon;
+        Random rnd = new Random();
+
+        public Form1(bool oyunModu = false)
+        {
+            InitializeComponent();
+            this.oyunModu = oyunModu;
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Size = new Size(600, 400);
+
+            //Label 
+            label1.Text = $"Puan: {puan}";
+            label1.Font = new Font("Arial", 14, FontStyle.Bold);
+            label1.AutoSize = true;
+            label1.Location = new Point(10, 10);
+
+            if (!oyunModu)
+            { 
+                //Ýlk form
+                this.Text = "Puan Ekraný (Sadece Label)";
+
+                //Oyun formu
+                Form1 oyunFormu = new Form1(true);
+                oyunFormu.Show();
+            }
+            else
+            {
+                
+                this.Text = "Oyun Ekraný (Butonlu)";
+
+                oyunButon = new Button();
+                oyunButon.Size = new Size(80, 80);
+                oyunButon.Font = new Font("Arial", 20, FontStyle.Bold);
+                oyunButon.FlatStyle = FlatStyle.Flat;
+                oyunButon.FlatAppearance.BorderSize = 0;
+                oyunButon.Click += OyunButon_Click;
+
+                this.Controls.Add(oyunButon);
+
+                timer1.Interval = 1000;
+                timer1.Tick += Timer1_Tick;
+                timer1.Start();
+
+                YeniKonumVeSayi();
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            YeniKonumVeSayi();
+        }
+
+        private void YeniKonumVeSayi()
+        {
+            int sayi = rnd.Next(1, 11);
+            oyunButon.Text = sayi.ToString();
+
+            int maxX = this.ClientSize.Width - oyunButon.Width;
+            int maxY = this.ClientSize.Height - oyunButon.Height;
+            int x = rnd.Next(0, maxX);
+            int y = rnd.Next(0, maxY);
+            oyunButon.Location = new Point(x, y);
+
+            if (ButonOrtadaMi(x, y))
+            {
+                oyunButon.BackColor = Color.Red;
+                oyunButon.ForeColor = Color.White;
+            }
+            else
+            {
+                oyunButon.BackColor = Color.Black;
+                oyunButon.ForeColor = Color.White;
+            }
+        }
+
+        private bool ButonOrtadaMi(int x, int y)
+        {
+            Rectangle ortaAlan = new Rectangle(
+                this.ClientSize.Width / 4,
+                this.ClientSize.Height / 4,
+                this.ClientSize.Width / 2,
+                this.ClientSize.Height / 2
+            );
+
+            Rectangle butonAlan = new Rectangle(x, y, oyunButon.Width, oyunButon.Height);
+            return ortaAlan.IntersectsWith(butonAlan);
+        }
+
+        private void OyunButon_Click(object sender, EventArgs e)
+        {
+            int sayi = int.Parse(oyunButon.Text);
+            if (oyunButon.BackColor == Color.Red)
+                puan += sayi;
+            else
+                puan -= sayi;
+
+            PuanGuncelle();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+        private void PuanGuncelle()
+        {
+            foreach (Form f in Application.OpenForms)
+            {
+                if (f is Form1 form)
+                {
+                    form.label1.Text = $"Puan: {puan}";
+                }
+            }
+        }
+
+        //Boþ designer eventleri için 
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
